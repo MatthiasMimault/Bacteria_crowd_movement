7196c83a521cd98859eeb6b0a646cc2940a56856
MatthiasMimault <34157729+MatthiasMimault@users.noreply.github.com>
2020-05-06 11:21:39 +0100

    Run code: release of V33

--- Exec/run_V33.m
+++ Exec/run_V33.m
@@ -10,92 +10,14 @@ clear
 
 %% 0. Settings
 % Names
-caseTitle = 'DAT-NbFilaments';
-caseDate = '0501';
-runnb = '2a';
-suffix = 'T100-LongerDomain';
-global T R Dx Dy nx CFL typeAt typeInit typeSrc typeObs typeVel A C epsilon...
-    BactValue dev Ro ro
+caseTitle = 'RUN-SimpleCase';
+caseDate = '0506';
+runnb = '6';
+suffix = 'T5-k005D001';
+global T Dx Dy nx CFL A D BactValue 
 
-% Physical parameters
-% T is time in s, A is bacteria average velocity in mm.s-1, C is diffusion 
-% coeff in mm.s-2, R is interaction radius in mm, epsilon is interaction 
-% strength, BactValue is in millions of bacteria per mL
-T = 100; A = 0.03; C = 0.001; 
-R = 0.5; epsilon = 0.1;
-BactValue = 20;
-% particles - porosity 65%
-Ro = 1.18; ro = 1;
-
-% Run parameters
-dev = 'release'; % stale, dev, release
-typeCase = 'custom';
-Nfiles = 10; 
-
-% Numerical parameters
-nx = 800;
-CFL = 1;
-ny = nx;
-
-% Case choice
-switch typeCase
-    case 'StatSol1D'
-        BactValue = 4*C/A; %1D
-        typeAt = 'none'; %up, root
-        typeSrc = 'none';
-        typeObs = 'none'; % none, particles
-        typeVel = 'leftright0'; % leftright0 static, att, att2, att-src, none, adv-src
-        typeInit = 'solstat1D'; % none square solstat1D
-        
-        % Dimensions
-        Domain = [-0.5, 0.5, -0.5, 0.5];
-        Space = Domain;
-        Attractant = [0 0 0 0]; %[-20 20 20 25] [-1 1 10 25]
-        InitBact = 0.5*Domain; %[-20, 10, -10, 10]
-        Source = [0 0 0 0]; 
-    case 'StatSol2D'
-        BactValue = 4*2*pi*C^2/A^2; %2D
-        typeAt = 'none'; %up, root
-        typeSrc = 'none'; %bottom, none
-        typeObs = 'none'; % none, particles
-        typeVel = 'centred'; % leftright0 static, att, att2, att-src, none, adv-src
-        typeInit = 'solstat2D'; % none square solstat2D
-        
-        % Dimensions
-        Domain = [-0.5, 0.5, -0.5, 0.5];
-        Space = Domain;
-        Attractant = [0 0 0 0]; %[-20 20 20 25] [-1 1 10 25]
-        InitBact = 0.5*Domain; %[-20, 10, -10, 10]
-        Source = [0 0 0 0]; 
-    case 'custom'
-        % User customisation here
-        typeAt = 'root'; %up, root
-        typeSrc = 'bottom'; %bottom, none
-        typeObs = 'none'; % none, particles
-        typeVel = 'att'; % static, att, att2, att-src, none, adv-src
-        typeInit = 'square'; % none square
-        
-        % Dimensions
-        Domain = [-4.25, 4.5, -10.2, 6];
-        Space = [-3.25, 3.5, -9.2, 5];
-        Attractant = [-0.2 0.2 4 6]; %[-20 20 20 25] [-1 1 10 25]
-        InitBact = [-3.25, 3.5, -9.2, 5];
-        Source = [-3.25, 3.5 -10.2 -9.2];
-    otherwise
-        % User customisation here
-        typeAt = 'root'; %up, root
-        typeSrc = 'bottom';
-        typeObs = 'particles'; % none, particles
-        typeVel = 'att'; % static, att, att2, att-src, none, adv-src
-        typeInit = 'square'; % none square
-        
-        % Dimensions
-        Domain = [-4.25, 4.5, -2.2, 6];
-        Space = [-3.25, 3.5, -1.2, 5];
-        Attractant = [-0.2 0.2 4 6]; %[-20 20 20 25] [-1 1 10 25]
-        InitBact = [-3.25, 3.5, -1.2, 5]; %[-20, 10, -10, 10]
-        Source = [-3.25, 3.5 -2.2 -1.2]; 
-end
+% input file
+input_V33
 
 %% Nothing to be modified below
 typeRepulsion = 0;
@@ -108,13 +30,13 @@ addpath('..\Cases')
     caseTitle, caseDate, runnb, nx, suffix);
 
 %% 1. Initialisation grid and dependant parameters
-[X,Y,Dx,Dy] = fGridGeneration(nx,ny,Domain);
+[X,Y,Dx,Dy] = fGridGeneration(nx,Domain);
 
 [domAt, domBd, domDef, domSrc] ...
     = fRegionGeneration(X, Y, Space, Attractant, Source);
 
 % Dt = min(min(Dx,Dy)/A/(1+epsilon),Dx*Dx/2/C)*CFL;
-Dt = min(Dx/A,Dx*Dx/2/C)*CFL;
+Dt = min(Dx/A,Dx*Dx/2/D)*CFL;
 disp(['Time step is ' num2str(Dt)]);
 tt = 0:Dt:T;
 TT = 0:T/Nfiles:T;
@@ -141,7 +63,7 @@ Axis = Space;
 
 % Save initial parameters
 save([dataRoot '\Data-' caseName '\' fileName '-init'],'domDef','domBd',...
-    'domAt', 'domSrc','A','C','Dx','Dt',...
+    'domAt', 'domSrc','A','D','Dx','Dt',...
     'Nt','T','Nfiles', 'Axis','dataRoot', 'fileName', 'caseName','nx')
 save([dataRoot '\Data-' caseName '\' fileName '-000'],...
      'X','Y','b','Dx','Dy','Vx','Vy','tsave')
